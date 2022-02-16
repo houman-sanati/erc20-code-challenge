8bbaf3f968929dcc344232a6093ec8ee0630b8e0
Houman <houmansnt@gmail.com>
2022-02-17 01:17:09 +0330

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Form, Row, Spinner } from "react-bootstrap";
 import { Strings } from "./util/Strings";
 import Table from "./components/Table";
 import tableColumns from "./data/tableColumns";
@@ -9,6 +9,8 @@ const App = () => {
 
   const [contractAddress, setcontractAddress] = useState('0x6b175474e89094c44da98b954eedeac495271d0f')
   const [eoaAddress, setEoaAddress] = useState('0xe5f8086dac91e039b1400febf0ab33ba3487f29a')
+
+  const [isLoading, setIsLoading] = useState(false)
 
   const [tableData, setTableData] = useState([])
 
@@ -22,15 +24,21 @@ const App = () => {
     }
     const tempTableData = [...tableData, item]
     setTableData(tempTableData)
+    setIsLoading(false)
   }
 
   const onSubmit = async () => {
+    if (!eoaAddress || !contractAddress) return
+    setIsLoading(true)
     const contractInstance = getContractInstance(contractAddress)
     Promise.all([
       contractInstance.balanceOf(eoaAddress),
       contractInstance.symbol(),
       contractInstance.name()
-    ]).then(results => addToTable(results[0], results[1], results[2]))
+    ]).then(results => addToTable(results[0], results[1], results[2])).catch(err => {
+      alert(Strings.invalidInput)
+      setIsLoading(false)
+    })
   }
 
   return (
@@ -42,27 +50,36 @@ const App = () => {
           </Container>
         </nav>
       </header>
-      <Container>
-        <Row className="mt-2">
-          <Col xs={12} md={4}>
-            <Form.Group className="mb-3">
-              <Form.Label>{Strings.contractAddress}</Form.Label>
-              <Form.Control value={contractAddress} onChange={(e) => setcontractAddress(e.target.value)} type="text" placeholder={Strings.contractHint} />
-            </Form.Group>
-          </Col>
-          <Col xs={12} md={4}>
-            <Form.Group className="mb-3">
-              <Form.Label>{Strings.eoaAddress}</Form.Label>
-              <Form.Control value={eoaAddress} onChange={(e) => setEoaAddress(e.target.value)} type="text" placeholder={Strings.eoaHint} />
-            </Form.Group>
-          </Col>
-          <Col xs={12} md={4} className='d-flex justify-content-center align-items-center'>
-            <Button className="bg-secondary border-secondary" type="submit" onClick={onSubmit}>{Strings.submit}</Button>
-          </Col>
-          <Col xs={12} className="mt-4">
-            <Table columns={tableColumns} data={tableData} />
-          </Col>
-        </Row>
+      <Container fluid>
+        <Card className="m-3">
+          <Card.Body>
+            <Row className="mt-2">
+              <Col xs={12} md={4}>
+                <Form.Group className="mb-3">
+                  <Form.Label>{Strings.contractAddress}</Form.Label>
+                  <Form.Control value={contractAddress} onChange={(e) => setcontractAddress(e.target.value)} type="text" />
+                </Form.Group>
+              </Col>
+              <Col xs={12} md={4}>
+                <Form.Group className="mb-3">
+                  <Form.Label>{Strings.eoaAddress}</Form.Label>
+                  <Form.Control value={eoaAddress} onChange={(e) => setEoaAddress(e.target.value)} type="text" />
+                </Form.Group>
+              </Col>
+              <Col xs={12} md={4} className='d-flex justify-content-center align-items-center'>
+                {isLoading ? <Spinner animation="grow" variant="primary" /> : <Button disabled={!eoaAddress || !contractAddress} className="bg-secondary border-secondary" type="submit" onClick={onSubmit}>{Strings.submit}</Button>}
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+        {tableData.length === 0 ? <div className="d-flex justify-content-center align-items-center mt-5">
+          <div>{Strings.addSomeData}</div>
+        </div> :
+          <Card className="m-3">
+            <Card.Body>
+              <Table columns={tableColumns} data={tableData} />
+            </Card.Body>
+          </Card>}
       </Container>
     </div>
   );
