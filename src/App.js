d0d64cd41ef63b244e09b1325869a05d6d0fe218
Houman <houmansnt@gmail.com>
2022-02-16 19:55:32 +0330

    form ui added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { Strings } from "./util/Strings";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className='sticky top-0 z-50'>
+        <nav className="navbar navbar-dark bg-primary">
+          <Container fluid>
+            <a href="/" className="navbar-brand">{Strings.appName}</a>
+          </Container>
+        </nav>
       </header>
+      <Container>
+        <Row className="mt-2">
+          <Col xs={12} md={4}>
+            <Form.Group className="mb-3">
+              <Form.Label>{Strings.contractAddress}</Form.Label>
+              <Form.Control type="text" placeholder={Strings.contractHint} />
+            </Form.Group>
+          </Col>
+          <Col xs={12} md={4}>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>{Strings.eoaAddress}</Form.Label>
+              <Form.Control type="text" placeholder={Strings.eoaHint} />
+            </Form.Group>
+          </Col>
+          <Col xs={12} md={4} className='d-flex justify-content-center align-items-center'>
+            <Button className="bg-secondary border-secondary" type="submit">{Strings.submit}</Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/util/Strings.js
@@ -0,0 +1,8 @@
+export const Strings = Object.freeze({
+    appName: 'Code Challenge',
+    contractAddress: 'Contract Address',
+    eoaAddress: 'EOA Address',
+    submit: 'Submit',
+    contractHint: '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984',
+    eoaHint: '0x7445F4B1F5aee39945Cbd1c34b0884ca008bf3d3'
+})
